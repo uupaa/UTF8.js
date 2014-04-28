2372fbc42f0562e9e4e46b9105fe11333b81f1b8
uupaa <uupaa.js@gmail.com>
2014-04-28 16:04:27 +0900

    follow NodeModule.js

--- test/index.node.js
+++ test/index.node.js
@@ -1,9 +1,9 @@
-require("uupaa.valid.js");
-require("uupaa.task.js");
-require("uupaa.test.js");
-require("uupaa.wordarray.js");
-require("uupaa.bytearray.js");
+require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
+require("../node_modules/uupaa.valid.js/lib/Valid.js");
+require("../node_modules/uupaa.task.js/lib/Task.js");
+require("../node_modules/uupaa.test.js/lib/Test.js");
+require("../node_modules/uupaa.bytearray.js/lib/ByteArray.js");
+require("../node_modules/uupaa.wordarray.js/lib/WordArray.js");
 require("../lib/UTF8.js");
 require("../release/UTF8.min.js");
 require("./test.js");
-
